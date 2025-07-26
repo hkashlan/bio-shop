f16094ec4cb9cd9ec057ad7eea92cbdc2b3500a8
Hadi Kashlan <hkashlan@gmail.com>
2025-07-26 18:29:40 +0200

    do some code

--- projects/provider/src/app/app.config.ts
+++ projects/provider/src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 import { routes } from './app.routes';
 
@@ -7,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(LucideAngularModule)
   ]
 };
--- projects/provider/src/app/pages/shell/shell.ts
+++ projects/provider/src/app/pages/shell/shell.ts
@@ -1,15 +1,33 @@
-
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import {  ActivityIcon, LucideAngularComponent, LucideAngularModule, ShoppingBasketIcon, UserRoundIcon, LogOutIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule, RouterLink],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
 export class ShellComponent {
+  menuItems = [
+    {
+      label: 'Dashboard',
+      icon: ActivityIcon,
+      route: '/dashboard',
+    },
+    {
+      label: 'Users',
+      icon: UserRoundIcon,
+      route: '/users',
+    },
+    {
+      label: 'Products',
+      icon: ShoppingBasketIcon,
+      route: '/products',
+    },
+  ];
+  logoutIcon = LogOutIcon;
 
   constructor(private router: Router) {}
 
